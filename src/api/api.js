dafdc0f6e86ef4a552641105e5e35863a6167c54
YASH MISHRA <100584516+YASHMISHRA22@users.noreply.github.com>
2024-03-04 22:48:46 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const apiKey = '6398147dfa02c7f0682a160d4cb12ac6';
+const apiKey = '';
 const getWeather = async (city) => {
     return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
     .then((res) => res.json())
