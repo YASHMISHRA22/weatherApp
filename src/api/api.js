b2083d588a56de70c0e6da8ba5b9bf2c8f8e4641
YASH MISHRA <yash2205misrha@gmail.com>
2024-03-04 22:44:40 +0530

    akashvani weather app

---
+++ src/api/api.js
@@ -0,0 +1,9 @@
+const apiKey = '6398147dfa02c7f0682a160d4cb12ac6';
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+export default getWeather;
